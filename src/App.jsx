925354c525261839e269ca444116c902b56c1596
Alberts Liepins <liepins01@gmail.com>
2022-06-15 19:25:26 +0300

    pre-setup for page creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import React from 'react';
-import MainLeftBar from '@React/MainLeftBar';
-import '@Style'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+// import MainLeftBar from '@React/MainLeftBar';
 
 class App extends React.Component{
-  render() { 
-    return ( 
-      <div className="App">
-        <MainLeftBar/>
-      </div>
-    )
+  render(){
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+  
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/">
+              <Register />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
